8805e24691f50cb9fe3b80a8a15ba142e0705133
ratanak-khoeurn <ratanak.khoeurn@student.passerellesnumeriques.org>
2024-08-30 16:32:28 +0700

    create next js project

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -14,7 +14,7 @@ const Portfolio = () => {
           <span className={styles.title}>Websites</span>
         </Link>
         <Link href="/portfolio/applications" className={styles.item}>
-          <span className={styles.title}>Application</span>
+          <span className={styles.title}>Applications</span>
         </Link>
       </div>
     </div>
